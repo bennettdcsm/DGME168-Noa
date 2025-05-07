262f5a83a664feec6c1afd5f6326dc73e95abee8
noamandorf <noatomasmandorf@smccd.edu>
2025-05-06 18:22:15 -0700

    upload portfolio test

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/DGME168-Noa'
+  build:{
+    rollupOptions: {
+      input:{
+        app: './main.html'
+      }
+    }
+  }
 })
