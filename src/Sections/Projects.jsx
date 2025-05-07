b7d42cb100d112ad330d5cf05751fbe4e0aa4f25
noamandorf <noatomasmandorf@smccd.edu>
2025-05-07 10:08:44 -0700

    3rd try with img bug

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -19,7 +19,7 @@ export default function Projects() {
     }
 
     return (
-        <section onMouseMove={handleMouseMove} className="relative c-space section-spacing sm:hidden" id='work'>
+        <section onMouseMove={handleMouseMove} className="relative c-space section-spacing" id='work'>
             <h2 className='text-heading'>My Work</h2>
             <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px]" />
             {myProjects.map((project) => (
@@ -30,7 +30,7 @@ export default function Projects() {
             {preview && (
 
                 <motion.img
-                    className='fixed top-0 left-0 z-50 object-cover h-56 rounded-lg shadow-lg pointer-events-none w-80'
+                    className='fixed top-0 left-0 z-50 object-cover h-56 rounded-lg shadow-lg pointer-events-none w-80 sm:hidden'
                     src={preview}
                     style={{x: springX, y: springY}}/>
             )}
